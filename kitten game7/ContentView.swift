9040452d59617f0523e06e82dc636039a0880028
evangelisq <angelangelis314@gmail.com>
2025-02-08 01:50:02 -0800

    Initial Commit

---
+++ kitten game7/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  kitten game7
+//
+//  Created by Evangelis Albesa on 2025-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var playerCard = "card11"
+    var cpuCard = "card12"
+    
+    var playScore = 0
+    var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack{
+            Spacer()
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            Spacer()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 90)
+                Spacer()
+                
+                HStack{
+                  
+                    Image("card11")
+                        .resizable()
+                        .frame( height:150)
+                    Spacer()
+                    Image("card12")
+                        .resizable()
+                        .frame(height:150)
+                    Spacer()
+                  
+                }
+                Spacer()
+                
+                  Button {
+                      deal()
+                  }label:{
+                    
+                      Image("button")
+                          .resizable()
+                          .aspectRatio(contentMode: .fit)
+                          .frame(width: 100, height: 100)
+            }
+                Spacer()
+            }
+            HStack{
+              
+                VStack{
+                    Spacer()
+                    Text("Player")
+                        .font(.headline)
+                        .padding(.bottom, 10.0)
+                    Text(String(playScore))
+                        .font(.largeTitle)
+                    
+                }
+                VStack{
+                    Spacer()
+                    Text("CPU")
+                        .font(.headline)
+                        .padding(.bottom, 10.0)
+                    Text(String(cpuScore))
+                        .font(.largeTitle)
+                }
+                
+            }
+            
+        }
+       
+        }
+    func deal(){
+        print("Deal Cards")    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View{
+        ContentView()
+    }
+}
---
+++ kitten game7/kitten_game7App.swift
@@ -0,0 +1,17 @@
+//
+//  kitten_game7App.swift
+//  kitten game7
+//
+//  Created by Evangelis Albesa on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct kitten_game7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
